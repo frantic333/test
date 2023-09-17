997e39b635f3448ed195452e15a198940f772f06
max <frantic33@yandex.ru>
2023-09-17 23:57:22 +0400

    Third lesson

--- lms_project/auth_app/models.py
+++ lms_project/auth_app/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from .functions import get_timestamp_path_user
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True,verbose_name='Email')
+    birthday = models.DateField(verbose_name='Дата рождения', null=True)
+    description = models.TextField(verbose_name='Обо мне', null=True, blank=True, default='', max_length=150)
+    avatar = models.ImageField(verbose_name='Фото', blank=True, upload_to=get_timestamp_path_user)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name_plural = 'Участники'
+        verbose_name = 'Участник'
+        ordering = ['last_name']
+
+    def __str__(self):
+        return f'Участник{self.first_name} {self.last_name}: {self.email}'
